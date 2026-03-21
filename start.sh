c89a65327582eda2870983a01e194a9f0407c3dc
J0cky300 <jackstawasz@live.com>
2026-03-21 13:03:38 -0400

    Add server startup script

---
+++ start.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Create venv if it doesn't exist
+if [ ! -d "venv" ]; then
+    printf "Creating virtual environment..."
+    python -m venv venv
+    printf " Done\n"
+fi
+printf "Activating virtual environment..."
+source venv/bin/activate
+printf " Done\n"
+
+# Only reinstall if requirements.txt has changed
+HASH_FILE="venv/.req_hash"
+REQ_PATH=$(find . -type f -iname "requirements.txt" | head -n 1)
+CURRENT_HASH=$(md5sum "$REQ_PATH" | awk '{print $1}')
+
+if [ ! -f "$HASH_FILE" ] || [ "$CURRENT_HASH" != "$(cat $HASH_FILE)" ]; then
+    printf "Installing dependencies..."
+    pip install -r "$REQ_PATH" --quiet
+    echo "$CURRENT_HASH" > "$HASH_FILE"
+    printf " Done\n"
+fi
+
+# Start the server
+printf "Starting server...\n"
+python src/app.py
